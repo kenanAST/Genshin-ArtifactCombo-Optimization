f029bb84906e42506f8db9e718c25e20fef186f1
kenannn <kenanjoshua3454@gmail.com>
2021-08-21 17:31:37 +0800

    Added List Stack for Artifact Type
    ArtifactType

--- main.java
+++ main.java
@@ -6,6 +6,56 @@ import java.util.*;
 
 class Main{
 
+    public static Stack<Artifact> flowerList(Artifact[] artifacts){
+        Stack<Artifact> flower = new Stack<Artifact>();
+        for(int i = 0; i<artifacts.length; i++){
+            if(artifacts[i].type.equals("Flower")){
+                flower.push(artifacts[i]);
+            }
+        }
+        return flower;
+    }
+
+    public static Stack<Artifact> plumeList(Artifact[] artifacts){
+        Stack<Artifact> plume = new Stack<Artifact>();
+        for(int i = 0; i<artifacts.length; i++){
+            if(artifacts[i].type.equals("Plume")){
+                plume.push(artifacts[i]);
+            }
+        }
+        return plume;
+    }
+
+    public static Stack<Artifact> eonList(Artifact[] artifacts){
+        Stack<Artifact> eon = new Stack<Artifact>();
+        for(int i = 0; i<artifacts.length; i++){
+            if(artifacts[i].type.equals("Sands")){
+                eon.push(artifacts[i]);
+            }
+        }
+        return eon;
+    }
+
+    public static Stack<Artifact> gobletList(Artifact[] artifacts){
+        Stack<Artifact> goblet = new Stack<Artifact>();
+        for(int i = 0; i<artifacts.length; i++){
+            if(artifacts[i].type.equals("Goblet")){
+                goblet.push(artifacts[i]);
+            }
+        }
+        return goblet;
+    }
+
+    public static Stack<Artifact> circletList(Artifact[] artifacts){
+        Stack<Artifact> circlet = new Stack<Artifact>();
+        for(int i = 0; i<artifacts.length; i++){
+            if(artifacts[i].type.equals("Circlet")){
+                circlet.push(artifacts[i]);
+            }
+        }
+        return circlet;
+    }
+
     public static Artifact stringtoArtifacts(String[] data){
         Artifact artifact = new Artifact(data[1], data[0], data[2]);
         for(int i = 9; i<data.length; i++){
@@ -98,7 +148,7 @@ class Main{
     public static Artifact[] readData(String path){
 
         String line = "";
-        Artifact[] artifacts = new Artifact[200];
+        Artifact[] artifacts = new Artifact[175];
         try{
             BufferedReader br = new BufferedReader(new FileReader(path));
 
@@ -128,7 +178,16 @@ class Main{
         String path = "D:/20s/1000DaysOfCode/Day 2/ArtifactOptimization/arts.csv";
 
         Artifact[] kenan = readData(path);
-        System.out.println(kenan[174].setName);
-        System.out.println(kenan[174].get_percentATK());
+        System.out.println(kenan[174].type);
+        System.out.println(kenan[174].type);
+
+        Stack<Artifact> flower = flowerList(kenan);
+        Stack<Artifact> plume = plumeList(kenan);
+        Stack<Artifact> eon = eonList(kenan);
+        Stack<Artifact> goblet = gobletList(kenan);
+        Stack<Artifact> circlet = circletList(kenan);
+        
+        System.out.println(flower.size() + " " + plume.size() + " " + eon.size() + " " + goblet.size() + " " + circlet.size());
+
     }
 }
